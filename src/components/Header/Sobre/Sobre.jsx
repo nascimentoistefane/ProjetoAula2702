58a5136c21d3686576ddbc728fb17595c8b01e12
Istefane Guilherme do Nascimento <art0066383@aluno.firjansenaisesi.com.br>
2026-02-27 15:33:52 -0300

    tutorial aula 2702

---
+++ src/components/Header/Sobre/Sobre.jsx
@@ -0,0 +1,13 @@
+import "./Sobre.css"
+
+export const Sobre = ({categoria}) => {
+    return(
+        <div id={categoria}>
+            <div id="resumo">
+                <h2>Sobre mim</h2>
+
+                <
+            </div>
+        </div>
+    )
+}
